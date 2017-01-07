4257a497d4cf4aa82ed007840518e762f3bd51c7
Belyj <Belyj_07@inbox.ru>
2017-01-07 14:35:52 +0400

    Сохдан список точек List<Point>

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,9 +12,18 @@ namespace Snake
         {
             Point point1 = new Point(1, 3, '*');
             point1.Draw();
-
             Point point2 = new Point(4, 5, '#');
             point2.Draw();
+            Point point3 = new Point(7, 10, '%');
+            point3.Draw();
+            Point point4 = new Point(4, 4, '$');
+            point4.Draw();
+
+            List<Point> pList = new List<Point>();
+            pList.Add(point1);
+            pList.Add(point2);
+            pList.Add(point3);
+            pList.Add(point4);
             Console.ReadLine();
         }
     }
