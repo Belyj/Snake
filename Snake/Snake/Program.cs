f31d9ce490d318cdb194345c95e28d79a0d945cb
Belyj <Belyj_07@inbox.ru>
2017-01-07 07:00:52 +0400

    Инкапмуляция, конструктор

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,16 +10,10 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point point1 = new Point();
-            point1.x = 1;
-            point1.y = 3;
-            point1.sym = '*';
+            Point point1 = new Point(1, 3, '*');
             point1.Draw();
 
-            Point point2 = new Point();
-            point2.x = 4;
-            point2.y = 5;
-            point2.sym = '#';
+            Point point2 = new Point(4, 5, '#');
             point2.Draw();
             Console.ReadLine();
         }
